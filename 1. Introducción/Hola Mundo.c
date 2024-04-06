07bab4db629fa910dfc9b8669bae41b6c9d0cbd3
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2024-04-06 14:41:06 -0600

    El famoso Hola Mundo

---
+++ 1. Introducción/Hola Mundo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>  // Librería estándar
+// Moises Hernandez Pacheco
+int main (){  // Función principal
+    /* Esto es
+    un comentario
+    de varias
+    líneas*/
+    printf("Hola Mundo!");
+    printf("\nHola amigos!");
+    return 0;
+}
