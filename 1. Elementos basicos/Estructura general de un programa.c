c1161ba4f4b163da6434e53527289e96360616ba
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2024-04-08 13:43:42 -0600

    Cambiando nombre de carpeta y agregando nuevo archivo

---
+++ 1. Elementos basicos/Estructura general de un programa.c
@@ -0,0 +1,27 @@
+#include <stdio.h>  // Importar librerias
+// stdio.h librería estandar de entrada y salida
+
+/* Codigo hecho por Moy el 15 de marzo del 2022
+ESCOM IPN MX */
+
+// printf() imprime cosas en la consola
+
+int main(){  // Funcion principal (necesaria)
+
+// Comentario de una sola linea xdxd
+
+/* Esto
+es un
+comentario
+de varias lineas
+
+sdvvdfdfddfsdfsdad
+xdxdxd
+*/
+	
+	// Imprime Hola Mundo con salto de linea \n (newline)
+	printf("\nHola Mundo");
+	printf("\nHola Mundo");
+	
+	return 0;
+}
