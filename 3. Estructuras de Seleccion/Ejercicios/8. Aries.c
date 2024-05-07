e7d60f71c5f63260418c839ed3d0172b4505b2f6
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2024-05-06 21:12:53 -0600

    Ejercicio con if y string.h

--- 3. Estructuras de Seleccion/Ejercicios/8. Aries.c
+++ 3. Estructuras de Seleccion/Ejercicios/8. Aries.c
@@ -2,10 +2,25 @@
 persona e imprima el nombre solo si la persona es signo Aries,
 en caso contrario, imprima "No es signo Aries" */
 #include <stdio.h>
+#include <string.h>
 
 int main(){
 
-    
+    char nombre[40], signo[15];
+
+    printf("Ingrese su nombre\n");
+    fgets(nombre,40,stdin);  // fgets(string, maximo de caracteres (para evitar una inyección), entrada);
+    strtok(nombre,"\n");  // strtok(string, delimitador);
+
+    printf("Ingrese su signo\n");
+    fgets(signo,15,stdin);
+    strtok(signo,"\n");
+
+    if (strcmp(signo,"aries") == 0 || strcmp(signo,"Aries") == 0)
+        printf("\nSu nombre es: %s",nombre);
+    else
+        printf("\nUsted no es Aries"); 
+
 
     return 0;
 }
