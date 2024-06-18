e0ddcb2ad6bd4375b34fd013c253d635c67f0b57
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2024-06-17 23:18:31 -0600

    Bucle For

---
+++ 4. Estructuras Repetitivas (Bucles)/2. Bucle For.c
@@ -0,0 +1,23 @@
+/* 
+Bucle For
+
+Sintaxis:
+
+for (inicialización; condición; incremento o decremento) {
+    Instrucciones
+}
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    int i;
+    
+    for(i = 0; i <= 10; i++){
+        printf("%i\n",i);
+    }
+
+
+    return 0;
+}
