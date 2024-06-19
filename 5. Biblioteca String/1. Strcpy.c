45225880027ab4f8f80bc7b03519ce60db0bd790
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2024-06-19 12:01:51 -0600

    Funcion Strcpy

---
+++ 5. Biblioteca String/1. Strcpy.c
@@ -0,0 +1,23 @@
+//* strcpy(cadenaDestino, cadenaOrigen);
+//* Permite copiar una cadena de texto en otra.
+//! Debes considerar que la dimensión del array destino debe ser igual o mayor que el array de origen
+//! y además sobrescribe el contenido del destino (de existir).
+
+#include <stdio.h>
+#include <string.h> //* Recordar importar librería String
+
+int main(){
+
+    char origen[] = "Programacion";
+    //* Dado que "Programación" tiene 12 caracteres
+    //* creamos el array Destino de la misma dimensión
+
+    char destino[12];
+
+    //! Copiamos el contenido y lo mostramos
+    strcpy(destino,origen);
+    printf("%s",destino);
+
+
+    return 0;
+}
