64cff6c8cb5dd93289c1f34a458d6eafdb623716
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2024-06-20 14:34:57 -0600

    Modificando Strrchr

--- 5. Biblioteca String/7. Strrchr.c
+++ 5. Biblioteca String/7. Strrchr.c
@@ -1,12 +1,12 @@
 //* strrchr(Cadena, Caracter);
-//* Regresa el RESTANTE de la cadena a partir de la primera aparición del caracter indicado (incluyendo al caracter).
+//* Regresa el RESTANTE de la cadena a partir de la ÚLTIMA aparición del caracter indicado (incluyendo al caracter).
 
 #include <stdio.h>
 #include <string.h>  //? No olvidar importar librería String
 
 int main(){
 
-    char abecedario[] = "abcdefghijklmnopqrstuvwxyz";
+    char abecedario[] = "abcdefgggghijklmnopqrstuvwxyz";
     char corte;
 
     printf("Ingrese la letra a partir de la cual se mostrara el abecedario:\t");
